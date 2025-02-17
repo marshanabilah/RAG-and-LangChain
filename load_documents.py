9f93f46e7d56b476ab348414451ff6ed62db4c32
Marsha Nabilah <58138825+marshanabilah@users.noreply.github.com>
2025-02-17 16:58:45 +0900

    add chromadb

--- load_documents.py
+++ load_documents.py
@@ -1,8 +1,19 @@
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import openai 
+import os
+import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+
 
 DATA_PATH = "data"
+CHROMA_PATH = "chroma"
 
 def main():
     generate_data_store()
@@ -10,6 +21,7 @@ def main():
 def generate_data_store():
     documents = load_documents()
     chunks = split_text(documents)
+    save_to_chroma(chunks)
 
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.md")
@@ -33,6 +45,19 @@ def split_text(documents: list[Document]):
     print(document.metadata)
 
     return chunks
+
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Create a new DB from the documents.
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
 
 if __name__ == "__main__":
     main()
